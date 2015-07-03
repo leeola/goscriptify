19ef9e28b7d7c8c5797ed25873d555fcbee829d4
Lee Olayvar <leeolayvar@gmail.com>
2015-07-03 14:34:13 -0700

    run: Added RunOneScriptOrDir

--- proc.go
+++ proc.go
@@ -31,6 +31,31 @@ func RunScript(p string) {
 	os.Exit(exit)
 }
 
+// RunDir compiles and runs the given go package directory with global
+// $args and default options - then Exits the process.
+//
+// IMPORTANT: This exits the process, captures Stdin, and prints to
+// Stdout and Stderr as needed.
+func RunDir(p string) {
+	opts := ScriptOptions{
+		"/tmp/goscriptify",
+		os.Stdin, os.Stdout, os.Stderr,
+	}
+	exit, err := RunScriptDirWithOpts(p, os.Args[1:], opts)
+	if err != nil {
+		if builderr, ok := err.(*BuildError); ok {
+			fmt.Fprint(os.Stderr, builderr.Error())
+		} else {
+			fmt.Fprintf(os.Stderr, "Fatal: %s", err.Error())
+		}
+
+		if exit == 0 {
+			os.Exit(1)
+		}
+	}
+	os.Exit(exit)
+}
+
 // RunOneScript will run the first given script that is found. Basically
 // a shorthand for FindScript and RunScript
 //
@@ -42,4 +67,28 @@ func RunOneScript(scripts ...string) {
 		log.Fatal("Fatal:", err.Error())
 	}
 	RunScript(s)
+}
+
+// RunOneScriptOrDir will run the first given script, or directory,
+// that is found. The "OrDir" version of RunOneScript.
+//
+// If useDir is true, scripts found in subdirectories will have their
+// immediate parent directory used as the build directory. As if
+// Go build was run with `go build .`. This allows you to match
+// a specific file, like `muta/muta.go` rather than just running the
+// directory blindly.
+//
+// IMPORTANT: This exits the process, captures Stdin, and prints to
+// Stdout and Stderr as needed.
+func RunOneScriptOrDir(useDir bool, paths ...string) {
+	s, isDir, err := FindScriptOrDir(paths, useDir)
+	if err != nil {
+		log.Fatal("Fatal:", err.Error())
+	}
+
+	if isDir {
+		RunDir(s)
+	} else {
+		RunScript(s)
+	}
 }
